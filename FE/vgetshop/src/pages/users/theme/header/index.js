edfa404e2dd26681dd7b36e2e409a300a6e51fbf
ttthuy04 <thuy0359255628thuy@gmail.com>
2025-02-26 21:43:57 +0700

    create header

--- FE/vgetshop/src/pages/users/theme/header/index.js
+++ FE/vgetshop/src/pages/users/theme/header/index.js
@@ -1,9 +1,57 @@
 import { memo } from "react";
+import { FaFacebook } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaTwitter } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import "./style.scss";
+
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <div className="header-top">
+      <div className="container">
+        <div className="row">
+          <div className="col-6 header-top-left">
+            <ul>
+              <li>
+                <MdOutlineEmail />
+                vgetshop@gmail.com
+              </li>
+              <li>Miễn phí ship đơn từ 50.000đ</li>
+            </ul>
+          </div>
+          <div className="col-6 header-top-right">
+            <ul>
+              <li>
+                <Link to={""}>
+                  {" "}
+                  <FaFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  {" "}
+                  <RiInstagramFill />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  {" "}
+                  <FaTwitter />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaRegUser />
+                </Link>
+                <span>Đăng nhập</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
